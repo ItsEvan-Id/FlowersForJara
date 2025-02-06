177f67a63fa437ca3681d84d79e5fdc30780d543
ItsEvan <efanjanuar12@gmail.com>
2025-02-06 16:31:31 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Happy Birthday Jaraa'.split('')
+const text = 'aku ada sesuatu buat kamu'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
